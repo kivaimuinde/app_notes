baaa81c27bbfa9cfab02d77dd822beecbc0613c1
muindejk001 <muindejosphat@gmail.com>
2020-08-29 10:01:04 +0300

    added the voter model

--- app/notes/models.py
+++ app/notes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -10,3 +11,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+class Voter(models.Model):
+    voter=models.OneToOneField(User, on_delete=models.CASCADE)
